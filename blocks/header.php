24dc13090a40e18f8abddef30fa43417aa0ff2f1
Yerkebulan-code <67746818+Yerkebulan-code@users.noreply.github.com>
2020-08-02 15:11:44 +0600

    Add files via upload

---
+++ blocks/header.php
@@ -0,0 +1,14 @@
+<header>
+	<div id="logo">
+		<a href="./" title="Басты бетке өту">Корона<span>вирус</span>.kz</a> 
+	</div>
+	<div id="menuHead">
+		<a href="news.php"><div style="margin-right: 5%">Жаңалықтар</div></a>
+		<a href="advice.php"><div style="margin-right: 5%">Кеңестер</div></a>
+		<a href="questions.php"><div>Cұрақтар</div></a>
+	</div>
+	<div id="regAuth">
+		<a href="reg.php">Регистрация </a> | 
+		<a href="auth.php">Авторизация
+	</div></a>
+</header>
---
+++ blocks/rightCol.php
@@ -0,0 +1,8 @@
+<div id="rightCol">
+	<a href="https://www.worldometers.info/coronavirus/" title="Әлемдік көрсеткіш" target="_blank"><div class="banner"> <img src="img/banner.png" alt="Banner 1" title="Әлемдік көрсеткіш">
+	</div>
+	<a href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019" title="ӘДСҰ" target="_blank"><div class="banner"> <img src="img/banner2.png" alt="Banner 2" title="ӘДСҰ">
+	</div>
+	<a href="https://www.coronavirus2020.kz/kz" title="Қазақстан ресми сайты" target="_blank"><div class="banner3"><img src="img/banner3.jpg" alt="Banner 3" title="Қазақстан ресми сайты">
+	</div></a>
+</div>
